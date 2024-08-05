6b61c83864aa2d61c8244416205303fd50e564a6
bbatyki <18826497930@163.com>
2024-08-05 11:46:54 +0800

    aa

--- src/App.vue
+++ src/App.vue
@@ -47,7 +47,7 @@ const options: MenuOption[] = [
   { key: 1, label: h('b', 'this is bold') },
   { key: 2, label: 'item2', disabled: true },
   { key: 3, label: 'item3', divided: true },
-  { key: 4, label: 'item4' }
+  { key: 4, label: 'item41' }
 ]
 </script>
 
